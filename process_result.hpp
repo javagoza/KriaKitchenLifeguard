a1f03bf6eb55648f54d382f8a6ff3514600ac45c
Enrique Albertos <enriquealbertos@hotmail.com>
2022-03-13 13:29:34 +0100

    Update process_result.hpp

--- process_result.hpp
+++ process_result.hpp
@@ -34,13 +34,13 @@ cv::Mat process_result(cv::Mat &m1, const vitis::ai::FaceDetectResult &result,
   strftime(timeStrBuffer, sizeof(timeStrBuffer), "%a %b %d %H:%M:%S %Y", &time);
   
   legend = "Unattended since:";
-  cv::putText(image,legend,cv::Point(1,50),cv::FONT_HERSHEY_PLAIN ,1,cv::Scalar(0,255,0),1,false);
+  cv::putText(image,legend,cv::Point(1,result.height - 24),cv::FONT_HERSHEY_PLAIN ,1,cv::Scalar(0,255,0),1,false);
   cv::rectangle(image,
-                  cv::Rect{cv::Point(0, 0),
-                           cv::Size{result.width, 16}},
-                  cv::Scalar(10,10,10),-1);
+                  cv::Rect{cv::Point(0, result.height - 24),
+                           cv::Size{result.width, 24}},
+                  cv::Scalar(30,30,30),-1);
   
-  cv::putText(image,timeStrBuffer,cv::Point(1,60),cv::FONT_HERSHEY_PLAIN ,1,cv::Scalar(255,0,0),1,false);
+  cv::putText(image,timeStrBuffer,cv::Point(1,result.height - 12),cv::FONT_HERSHEY_PLAIN ,1,cv::Scalar(255,0,0),1,false);
 
   for (const auto &r : result.rects) {
     LOG_IF(INFO, is_jpeg) << " " << r.score << " "  //
