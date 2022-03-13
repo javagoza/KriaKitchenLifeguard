36960a07a7841188f47af32283e76c49c0218d8e
Enrique Albertos <enriquealbertos@hotmail.com>
2022-03-13 17:30:18 +0100

    Update process_result.hpp

--- process_result.hpp
+++ process_result.hpp
@@ -31,15 +31,18 @@ cv::Mat process_result(cv::Mat &m1, const vitis::ai::FaceDetectResult &result,
   struct tm lastTimeTm = *localtime(&lastTimeSeenPeople); 
   struct tm timeTm = *localtime(&actualTime);
   
-  strftime(timeStrBuffer, sizeof(timeStrBuffer), "Time: %H:%M:%S", &timeTm);
-  strftime(timeStrBuffer, sizeof(timeStrBuffer), "Unatt. since: %H:%M:%S", &lastTimeTm);
+ 
+  
   cv::rectangle(image,
                   cv::Rect{cv::Point(0, result.height - 24),
                            cv::Size{result.width, 24}},
                   cv::Scalar(30,30,30),-1);
   
+  strftime(timeStrBuffer, sizeof(timeStrBuffer), "Time: %H:%M:%S", &timeTm);
   cv::putText(image,timeStrBuffer,cv::Point(1,result.height - 9),cv::FONT_HERSHEY_PLAIN ,1,cv::Scalar(255,255,255),1,false);
-  cv::putText(image,timeStrBuffer,cv::Point(500,result.height - 9),cv::FONT_HERSHEY_PLAIN ,1,cv::Scalar(255,0,0),1,false);
+  
+  strftime(timeStrBuffer, sizeof(timeStrBuffer), "Unatt. since: %H:%M:%S", &lastTimeTm);
+  cv::putText(image,timeStrBuffer,cv::Point(400,result.height - 9),cv::FONT_HERSHEY_PLAIN ,2,cv::Scalar(0,255,0),1,false);
 
   for (const auto &r : result.rects) {
     lastTimeSeenPeople = actualTime;
