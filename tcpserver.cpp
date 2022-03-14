b3f8a80f60a9532b6e51e31c9cd5702c1f5f1ba8
Enrique Albertos <enriquealbertos@hotmail.com>
2022-03-14 10:47:20 +0100

    Update tcpserver.cpp

--- tcpserver.cpp
+++ tcpserver.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
         connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
 
         ticks = time(NULL);
-        memset(buf, 0, sizeof(sendBuff));
+        memset(sendBuff, '0', sizeof(sendBuff));
         int readLenght = readData(sendBuff, sizeof(sendBuff));
         // snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
         write(connfd, sendBuff, strlen(sendBuff)); 
@@ -52,68 +52,6 @@ int main(int argc, char *argv[])
      }
 }
 
-int main2 (int argc, char *argv[]) {
-   int sockfd, sockfd_client;
-   int ret = 0;
-   socklen_t sock_pkt_size = 0;
-   char buf[BUF_SIZE];
-   
-   struct sockaddr_in server_addr, client_addr;
-    
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
-   if (sockfd != -1) {
-        printf ("Socket creation success : sockfd = %d \n", sockfd);
-   } else {
-        perror("socket sockfd");        
-        return -1;
-   }  
-   
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_port = htons(8000);
-   server_addr.sin_addr.s_addr = inet_addr("192.168.2.95");     
-   ret = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in));
-   if (ret == 0)  {
-        printf (" Bind Success \n");
-   } else  {
-        perror("Bind");        
-        return -1;
-   }    
-   while(1) {
-      ret = listen(sockfd, 5);
-      if (ret == 0) {
-           printf (" Listen Success \n");
-      } else {
-           perror("listen");        
-           return -1;
-      }    
-
-      sock_pkt_size = sizeof(struct sockaddr_in);
-
-      sockfd_client = accept(sockfd, (struct sockaddr *)&client_addr, &sock_pkt_size);
-      if (sockfd_client != -1) {
-           printf ("Socket creation success : sockfd_client = %d \n", sockfd_client);
-      } else {
-           perror("socket sockfd_client");        
-           return -1;
-      }
-      
-      memset(buf, 0, sizeof(buf));
-      int readLenght = readData(buf, sizeof(buf));
-      ret = send(sockfd_client, buf, readLenght, 0);
-      if(ret == -1) {
-           perror("Socket send");
-      } else {
-           printf ("Message sent to client:- %s : size = %d\n", buf, ret); 
-      }
-      memset(buf, 0, sizeof(buf));
-      ret = recv(sockfd_client, buf, sizeof(buf), 0);
-      if(ret == -1) {
-           perror("Socket recv");
-      } else {
-           printf ("Message received from client :- %s : size = %d\n", buf, ret); 
-      }   
-   }   
- }
 
 
 void report_and_exit(const char* msg) {
@@ -150,7 +88,6 @@ int readData(char* buffer, int bufferSize) {
   int c; /* buffer for read bytes */
   int i = 0;
   while (read(fd, &c, 1) > 0) {   /* 0 signals EOF */
-    //write(STDOUT_FILENO, &c, 1); /* write one byte to the standard output */
     buffer[i++] = c;
   }
    buffer[i] = '\0';
