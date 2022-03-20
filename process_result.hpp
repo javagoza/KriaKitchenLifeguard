2930b522cf4dc478ae717f625c76c96fffd2a291
Enrique Albertos <enriquealbertos@hotmail.com>
2022-03-20 20:11:00 +0100

    Update process_result.hpp

--- process_result.hpp
+++ process_result.hpp
@@ -34,6 +34,8 @@ cv::Mat process_result(cv::Mat &m1, const vitis::ai::FaceDetectResult &result,
   char timeStrBuffer[256];
   char unattendedTimeStrBuffer[256];
   char dataBuffer[256];
+  char buffer[100];
+  double diff_t;
   
   static time_t lastTimeSeenPeople;
   
@@ -43,6 +45,7 @@ cv::Mat process_result(cv::Mat &m1, const vitis::ai::FaceDetectResult &result,
   
   struct tm lastTimeTm = *localtime(&lastTimeSeenPeople); 
   struct tm timeTm = *localtime(&actualTime); 
+  diff_t = difftime(actualTime, lastTimeSeenPeople);
   
   cv::rectangle(image,
                   cv::Rect{cv::Point(0, result.height - 24),
@@ -58,7 +61,9 @@ cv::Mat process_result(cv::Mat &m1, const vitis::ai::FaceDetectResult &result,
   // pass message with last seen time through shared file
   if( result.rects.size() > 0 ) {
     lastTimeSeenPeople = actualTime;
-    strftime(dataBuffer, sizeof(dataBuffer), "%Y-%m-%d %H:%M:%S", &lastTimeTm);
+    strftime(timeStrBuffer, sizeof(timeStrBuffer), "%Y-%m-%d %H:%M:%S", &lastTimeTm);
+    sprintf(dataBuffer, "%s|%f", timeStrBuffer,diff_t);
+    
     writeLastTimeSeen(dataBuffer);
   }
      
